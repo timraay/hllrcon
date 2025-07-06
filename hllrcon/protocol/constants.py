2f6df4b2f9a9d6b5442fa48398299aa083e033eb
Tim Raaymakers <43066279+timraay@users.noreply.github.com>
2025-07-06 14:50:24 +0200

    Update protocol rules

--- hllrcon/protocol/constants.py
+++ hllrcon/protocol/constants.py
@@ -1,8 +1,8 @@
 from typing import Final
 
-DO_ALLOW_CONCURRENT_REQUESTS: Final[bool] = False
-DO_POP_V1_XORKEY: Final[bool] = True
-DO_USE_REQUEST_HEADERS: Final[bool] = False
+DO_ALLOW_CONCURRENT_REQUESTS: Final[bool] = True
+DO_POP_V1_XORKEY: Final[bool] = False
+DO_USE_REQUEST_HEADERS: Final[bool] = True
 DO_XOR_RESPONSES: Final[bool] = False
 
 HEADER_FORMAT: Final[str] = "<II"
